ecf7494465cbd1fa7d59555950ed58bb68d2d02b
allenyhu@vlab.usc.edu <allenyhu@vlab.usc.edu>
2018-04-02 14:27:59 -0700

    replace with solution and add serversocket

--- src/com/chunkserver/ChunkServer.java
+++ src/com/chunkserver/ChunkServer.java
@@ -1,128 +1,106 @@
 package com.chunkserver;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.ServerSocket;
-import java.util.Scanner;
+import java.util.Arrays;
+//import java.util.Arrays;
 
 import com.interfaces.ChunkServerInterface;
 
 /**
  * implementation of interfaces at the chunkserver side
- * 
  * @author Shahram Ghandeharizadeh
  *
  */
 
 public class ChunkServer implements ChunkServerInterface {
-	final static String filePath = "U:\\Desktop\\allenyhu_csci485Disk\\"; // or C:\\newfile.txt
+	final static String filePath = "csci485/";	//or C:\\newfile.txt
 	public static long counter;
 	private ServerSocket ss;
+	
 	/**
 	 * Initialize the chunk server
 	 */
-	public ChunkServer(int port) {
-		this.ss = new ServerSocket(port);
-		
+	public ChunkServer(int port){
 		File dir = new File(filePath);
-		counter = -1;
+		File[] fs = dir.listFiles();
+
+		if(fs.length == 0){
+			counter = 0;
+		}else{
+			long[] cntrs = new long[fs.length];
+			for (int j=0; j < cntrs.length; j++)
+				cntrs[j] = Long.valueOf( fs[j].getName() ); 
+			
+			Arrays.sort(cntrs);
+			counter = cntrs[cntrs.length - 1];
+		}
 		try {
-			if(!dir.exists()) { //directory doesn't exist
-				dir.mkdir();
-				counter = 0;
-				FileWriter writer = new FileWriter(filePath + "counter.txt");
-				BufferedWriter bwriter = new BufferedWriter(writer);
-				bwriter.write(Long.toString(counter));
-				bwriter.close();
-				writer.close();
-			} else { //directory already exists
-				FileReader fr = new FileReader(filePath + "counter.txt");
-				BufferedReader br = new BufferedReader(fr);
-				counter = Long.valueOf(br.readLine());
-				br.close();
-				fr.close();
-			}
-		} catch (Exception ioe) {
-			System.out.println("error in chunkserver const");
-			System.out.println("error: " + ioe.getMessage());
-		}
-		
-		while(true) {
-			try {
+			ss = new ServerSocket(port);
+			while(true) {
 				ss.accept();
-			} catch (IOException ioe) {
-				System.out.println("ss ioe: " + ioe.getMessage());
 			}
-		}
+		} catch (IOException ioe) {
+			System.out.println("server socket ioe: " + ioe.getMessage());
+		} 
 		
 	}
-
+	
 	/**
-	 * Each chunk corresponds to a file. Return the chunk handle of the last chunk
-	 * in the file.
+	 * Each chunk is corresponding to a file.
+	 * Return the chunk handle of the last chunk in the file.
 	 */
 	public String initializeChunk() {
-//		System.out.println("createChunk invoked:  Part 1 of TinyFS must implement the body of this method.");
-//		System.out.println("Returns null for now.\n");
-		File chunk = new File(filePath + counter);
 		counter++;
-		try {
-			FileWriter writer = new FileWriter(filePath + "counter.txt");
-			BufferedWriter bwriter = new BufferedWriter(writer);
-			bwriter.write(Long.toString(counter));
-			bwriter.close();
-			writer.close();
-		} catch (IOException ioe) {
-			System.out.print("ioe in initializeChunk: " + ioe.getMessage());
-		}
-		return chunk.getPath();
+		return String.valueOf(counter);
 	}
-
+	
 	/**
-	 * Write the byte array to the chunk at the specified offset The byte array size
-	 * should be no greater than 4KB
+	 * Write the byte array to the chunk at the offset
+	 * The byte array size should be no greater than 4KB
 	 */
 	public boolean putChunk(String ChunkHandle, byte[] payload, int offset) {
 		try {
-			RandomAccessFile writer = new RandomAccessFile(ChunkHandle, "rw");
-		    writer.seek(offset);
-		    writer.write(payload);
-		    writer.close();
-		} catch (IOException ioe) {
-			System.out.println("Failed to putChunk");
-			System.out.println("error: " + ioe.getMessage());
+			//If the file corresponding to ChunkHandle does not exist then create it before writing into it
+			RandomAccessFile raf = new RandomAccessFile(filePath + ChunkHandle, "rw");
+			raf.seek(offset);
+			raf.write(payload, 0, payload.length);
+			raf.close();
+			return true;
+		} catch (IOException ex) {
+			ex.printStackTrace();
 			return false;
 		}
-		return true;
 	}
-
+	
 	/**
 	 * read the chunk at the specific offset
 	 */
 	public byte[] getChunk(String ChunkHandle, int offset, int NumberOfBytes) {
-		byte[] result = new byte[NumberOfBytes];
 		try {
-		    RandomAccessFile reader = new RandomAccessFile(ChunkHandle, "r");
-		    reader.read(result, offset, NumberOfBytes);
-		    reader.close();
-		} catch (IOException ioe) {
-			System.out.println("Failed getChunk");
-			System.out.println("error: " + ioe.getMessage());
+			//If the file for the chunk does not exist the return null
+			boolean exists = (new File(filePath + ChunkHandle)).exists();
+			if (exists == false) return null;
+			
+			//File for the chunk exists then go ahead and read it
+			byte[] data = new byte[NumberOfBytes];
+			RandomAccessFile raf = new RandomAccessFile(filePath + ChunkHandle, "rw");
+			raf.seek(offset);
+			raf.read(data, 0, NumberOfBytes);
+			raf.close();
+			return data;
+		} catch (IOException ex){
+			ex.printStackTrace();
 			return null;
 		}
-	    return result;
 	}
 	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter a port number: ");
-		int port = scan.nextInt();
-		ChunkServer server = new ChunkServer(port);
+	public static void maint(String[] args) {
+		
 	}
+	
+	
 
 }
